30229190080f2bb61d17c8695bc6b385c10e3c2f
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-08-27 19:22:16 +0300

    offloading the header code to other components

---
+++ resources/views/components/header/header.blade.php
@@ -0,0 +1,6 @@
+<div class="header__main bg-white text-grey text-2xl py-4 relative z-10 shadow-md">
+    {{-- Large screens --}}
+<x-header.large-screens />
+    {{-- Small screens --}}
+<x-header.small-screens />
+</div>
