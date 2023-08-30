4f914104e6476952a3df63f4cc6f667b5a08ff68
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-08-30 16:57:48 +0300

    Changing text styling

--- resources/views/components/homePage/openSection.blade.php
+++ resources/views/components/homePage/openSection.blade.php
@@ -8,8 +8,8 @@
     </div>
     <div class="text-center mt-5 lg:pr-10">
         <img src="https://res.cloudinary.com/dp0m5mp1s/image/upload/v1674554370/IkiSkanaus/kursinis_logo_yfv2zl.png" alt="Logotipas" class="lg:ml-4 shadow-lg">
-        <p class="mt-6 text-4xl text-red-600 font-bold relative one"><span>MŪSŲ SKANIAUSIEJI KEBABAI</span>
-            <span class="absolute -bottom-1 left-0 w-0 transition-all h-1 bg-red-600 mb-2"></span>
+        <p class="mt-6 text-4xl text-white font-bold relative one"><span>MŪSŲ SKANIAUSIEJI KEBABAI</span>
+            <span class="absolute -bottom-1 left-0 w-0 transition-all h-1 bg-red-400 mb-2"></span>
         </p>
         <a href="/meniu">
             <button class="mt-2 px-4 py-2 text-lg mb-3 text-red-200 border border-red-500 rounded-full hover:bg-red-500 hover:text-white transition duration-300">Spausk čia</button>
