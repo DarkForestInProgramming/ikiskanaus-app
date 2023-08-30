fd307971e06c1349e23a01a15ffefb40db356961
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-08-30 16:56:30 +0300

    Changing footer info

--- resources/views/components/footer/footer.blade.php
+++ resources/views/components/footer/footer.blade.php
@@ -1,9 +1,9 @@
 <div class="footer__main bg-red-200 text-red-500 py-6 text-center">
     <div class="container mx-auto">
         <h3 class="text-3xl mb-2 font-semibold">Nori kebabo?</h3>
-        <p class="mb-2 text-4xl font-bold">+37067229178</p>
+        <p class="mb-2 text-4xl font-bold">+37068988893</p>
         <p class="mb-4">
-            <a href="https://www.google.com/maps/place/Klaip%C4%97dos+g.+26,+Panev%C4%97%C5%BEys" class="text-xl font-semibold" target="_blank" rel="noopener noreferrer">Klaipėdos g. 26, Panevėžys</a>
+            <a href="https://www.google.com/maps/place/Laisv%C4%97s+a.+23,+Panev%C4%97%C5%BEys+35200" class="text-xl font-semibold" target="_blank" rel="noopener noreferrer">Kriponio g. 33, Panevėžys</a>
         </p>
         <p class="text-md">&copy; {{ date('Y') }} IkiSkanaus</p>
     </div>
