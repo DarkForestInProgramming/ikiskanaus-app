afe74829433cf0cded8e43b91728aa79e18910f6
DarkForrestInProgramming <deividasprogrameris@gmail.com>
2023-08-30 16:57:54 +0300

    Changing location

--- resources/views/components/homePage/locationSection.blade.php
+++ resources/views/components/homePage/locationSection.blade.php
@@ -6,13 +6,13 @@
         </div>
         <div class="w-full md:w-1/3 md:pl-4">
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2230.258728226495!2d24.355006316058967!3d55.74388438055907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e75a631a44f9e7%3A0xb75235bace9992d1!2sKlaip%C4%97dos%20g.%2026%2C%20Panev%C4%97%C5%BEys%2015109!5e0!3m2!1sen!2slt!4v1693445642107!5m2!1sen!2slt"
-                width="100%"
-                height="510"
-                style="border:0;"
-                allowfullscreen=""
-                loading="lazy"
-            ></iframe>
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2230.258728226495!2d24.355006316058967!3d55.74388438055907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e75a631a44f9e7%3A0xb75235bace9992d1!2sLaisv%C4%97s%20a.%2023%2C%2035200%20Panev%C4%97%C5%BEys!5e0!3m2!1sen!2slt!4v1693445642107!5m2!1sen!2slt"
+            width="100%"
+            height="510"
+            style="border:0;"
+            allowfullscreen=""
+            loading="lazy"
+        ></iframe>
         </div>
     </div>
 </div>
